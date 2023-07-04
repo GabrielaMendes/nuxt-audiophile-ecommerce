a857cbf509a766e70a3cd0301608d112a04b6ead
Gabriela <gdemelo.mendes@gmail.com>
2023-07-04 18:47:34 -0300

    layout: created TheNavbar component responsive layout

--- app.vue
+++ app.vue
@@ -1,3 +1,6 @@
 <template>
-  <div></div>
+	<div>
+    <TheNavbar />
+  </div>
 </template>
+
---
+++ components/TheNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="bg-black text-white">
+		<div class="content-container py-8 flex gap-10 justify-between">
+			<button class="lg:hidden">
+				<IconHamburger />
+			</button>
+			<NuxtLink to="/" class="sm:max-lg:mr-auto">
+				<IconLogo />
+			</NuxtLink>
+			<nav class="flex gap-8 max-lg:hidden">
+				<NuxtLink to="/">Home</NuxtLink>
+				<NuxtLink to="/">Headphones</NuxtLink>
+				<NuxtLink to="/">Speaker</NuxtLink>
+				<NuxtLink to="/">Eaphones</NuxtLink>
+			</nav>
+			<button>
+				<IconCart />
+			</button>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+a {
+	@apply text-xs uppercase hover:text-terracotta transition-colors duration-200;
+}
+</style>
