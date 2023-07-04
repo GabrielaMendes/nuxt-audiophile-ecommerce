1658f6245ce73c22f751a063976e0a0e2040487d
Gabriela <gdemelo.mendes@gmail.com>
2023-07-04 16:44:17 -0300

    config: installed google-fonts module and added project font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss"
-  ]
-})
+	devtools: { enabled: true },
+	modules: [
+		"@nuxtjs/tailwindcss",
+		[
+			"@nuxtjs/google-fonts",
+			{
+				families: {
+					Manrope: [400, 500, 700],
+				},
+			},
+		],
+	],
+});
