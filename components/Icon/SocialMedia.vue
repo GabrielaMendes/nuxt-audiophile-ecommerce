6c374dacf4f21c883b6a3ac86096f2838d0ed279
Gabriela <gdemelo.mendes@gmail.com>
2023-07-04 21:22:28 -0300

    feature and refactor: made social media svg icons change colors on hover and refactored code for reusability

---
+++ components/Icon/SocialMedia.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  iconHeight: String,
+  iconPath: String,
+})
+
+const isHovering = ref(false);
+</script>
+
+<template>
+  <svg
+		@mouseenter="isHovering = true"
+		@mouseleave="isHovering = false"
+		width="24"
+		:height="iconHeight"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			class="transition-colors duration-200 ease-linear"
+			:class="isHovering ? 'fill-terracotta' : 'fill-white'"
+			:d="iconPath"
+			fill-rule="nonzero"
+		/>
+	</svg>
+</template>
