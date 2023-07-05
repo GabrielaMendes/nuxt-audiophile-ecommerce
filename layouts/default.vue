cb20a1ba659003acd1a56b462492b228bf03633f
Gabriela <gdemelo.mendes@gmail.com>
2023-07-05 10:41:22 -0300

    layout: created website default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<TheNavbar class="sticky top-0" />
+
+    <header class="bg-almost-black text-white">
+      <div class="content-container">
+        <slot name="header"></slot>
+      </div>
+    </header>
+
+		<main class="content-container">
+			<slot name="content"></slot>
+    </main>
+
+		<TheFooter />
+	</div>
+</template>
+
+<style scoped>
+header :slotted(div) {
+  @apply border-t-[1px] border-white border-opacity-20
+}
+</style>
