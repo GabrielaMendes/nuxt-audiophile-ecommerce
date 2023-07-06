c586e254968dd9e20a97dcf721565be8f19fdc54
Gabriela <gdemelo.mendes@gmail.com>
2023-07-06 18:36:52 -0300

    fix: fixed unfilled  BaseButton color behavior

--- components/Base/Button.vue
+++ components/Base/Button.vue
@@ -21,7 +21,7 @@
 		:class="{
       'bg-almost-black text-white hover:bg-dark-gray': filled && black,
       'bg-terracotta text-white hover:bg-light-salmon': filled && !black,
-      'border-[1px] border-almost-black hover:bg-almost-black text-white': !filled,
+      'border-[1px] border-almost-black hover:bg-almost-black hover:text-white': !filled,
     }"
 	>
 		{{ text }}
