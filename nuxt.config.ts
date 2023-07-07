f94ef721c54b5db8ecfab83b8dd1b799a2f7a9a5
Gabriela <gdemelo.mendes@gmail.com>
2023-07-07 14:50:34 -0300

    config: added tailwindcss and autoprefixer to postcss.pllugins object

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ["/assets/main.css"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
 	modules: [
 		"@nuxtjs/tailwindcss",
 		"@vueuse/nuxt",
