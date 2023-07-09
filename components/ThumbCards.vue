a7a9bb1e7782bcb9ea8a9b0139a24f37dfdc13a1
Gabriela <gdemelo.mendes@gmail.com>
2023-07-09 15:42:47 -0300

    feature: made link between thumb cards and respective products pages

--- components/ThumbCards.vue
+++ components/ThumbCards.vue
@@ -20,22 +20,21 @@ const thumbs = [
 		<div
 			v-for="thumb in thumbs"
 			:key="thumb.name"
+			@click="navigateTo(`/products/${thumb.name.toLowerCase()}`)"
 			class="relative group cursor-pointer bg-very-light-gray pb-6 rounded-md text-center flex-1 motion-safe:md:hover:shadow-md motion-safe:md:hover:scale-[1.03] transition-all duration-500 ease-out"
 		>
 			<img
 				:src="thumb.image"
 				:alt="`${thumb.name} thumbnail`"
 				class="w-56 mx-auto"
-        :class="{
-          '-mt-20': thumb.name === 'Headphones',
-          '-mt-[4.5rem]': thumb.name === 'Speakers',
-          '-mt-16': thumb.name === 'Earphones',
-        }"
+				:class="{
+					'-mt-20': thumb.name === 'Headphones',
+					'-mt-[4.5rem]': thumb.name === 'Speakers',
+					'-mt-16': thumb.name === 'Earphones',
+				}"
 			/>
 			<h6 class="-mt-3 mb-2">{{ thumb.name }}</h6>
-			<button
-				class="mx-auto flex items-center transition-all duration-200"
-			>
+			<button class="mx-auto flex items-center transition-all duration-200">
 				<span
 					class="text-xs opacity-50 group-hover:opacity-100 group-hover:text-terracotta font-normal uppercase mr-3"
 					>Shop</span
