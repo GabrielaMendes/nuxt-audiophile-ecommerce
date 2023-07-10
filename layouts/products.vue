b97665003e4b445016ad8e58669fb4f0f2a744b1
Gabriela <gdemelo.mendes@gmail.com>
2023-07-10 17:34:01 -0300

    layout: created new page layout

---
+++ layouts/products.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<TheNavbar />
+
+		<header class="bg-almost-black text-white">
+			<div class="content-container">
+				<slot name="header"></slot>
+			</div>
+		</header>
+
+		<main class="content-container">
+			<slot name="content"></slot>
+      
+      <section class="pt-24 lg:pt-14">
+				<ThumbCards />
+			</section>
+		</main>
+
+		<BestGear />
+		<TheFooter />
+	</div>
+</template>
