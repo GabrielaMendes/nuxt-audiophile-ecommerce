e6f16ee24eea63f4f08260e6da5fcf40551bdbbc
Gabriela <gdemelo.mendes@gmail.com>
2023-07-17 21:04:17 -0300

    feature: created cart modal and items comopnents

---
+++ components/Cart/Item.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+	item: Object,
+});
+
+const { addItem, removeItem } = useCartStore();
+</script>
+
+<template>
+	<div class="flex items-center justify-between">
+		<div class="flex items-center gap-4">
+			<NuxtImg
+				:src="item.images.mobile.product"
+				alt="Product picture"
+				class="w-16 rounded-lg"
+			/>
+			<div class="font-bold">
+				<div>
+					{{
+						item.name
+							.replace("Headphones", "")
+							.replace("Earphones", "")
+							.replace("Speaker", "")
+							.replace("Mark", "MK")
+					}}
+				</div>
+				<p>$ {{ item.price.toLocaleString() }}</p>
+			</div>
+		</div>
+		<div class="bg-very-light-gray h-9 shrink-0">
+			<button @click="removeItem(item.id)" class="number-button">-</button>
+			<input
+				:value="item.qty"
+				type="number"
+				min="1"
+				max="10"
+				step="1"
+				class="h-full w-7 text-center bg-very-light-gray"
+			/>
+			<button @click="addItem(item.id, 1)" class="number-button">+</button>
+		</div>
+	</div>
+</template>
---
+++ components/Cart/Modal.vue
@@ -0,0 +1,35 @@
+<script setup>
+const cartStore = useCartStore();
+const { cartItems, totalItems, totalPrice } = storeToRefs(cartStore);
+const { removeAll } = cartStore;
+</script>
+
+<template>
+	<div class="ml-auto py-6 max-sm:mx-auto max-w-[28rem] max-h-screen">
+		<div
+			class="rounded-lg w-full overflow-y-auto bg-off-white content-container p-8"
+		>
+			<!-- Header -->
+			<div class="flex justify-between mb-8">
+				<h6>Cart ({{ totalItems }})</h6>
+				<button
+					@click="removeAll"
+					class="opacity-50 underline hover:opacity-100 hover:text-terracotta"
+				>
+					Remove all
+				</button>
+			</div>
+			<!-- Items -->
+			<div v-for="item in cartItems" :key="item.id" class="mt-6">
+				<CartItem :item="item" />
+			</div>
+			<!-- Price -->
+			<div class="flex justify-between mt-8">
+				<p class="uppercase">Total</p>
+				<h6>$ {{ totalPrice.toLocaleString() }}</h6>
+			</div>
+			<!-- Checkout Button -->
+			<BaseButton text="Checkout" class="w-full mt-6" />
+		</div>
+	</div>
+</template>
