a6ed023c41585598941bd50b5aae395da1d94009
Gabriela <gdemelo.mendes@gmail.com>
2023-07-20 17:34:13 -0300

    chore: removed unused code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
-import { acceptHMRUpdate } from "pinia";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ["~/assets/main.css"],
 	app: {
 		pageTransition: { name: "page", mode: "out-in" },
-		//layoutTransition: { name: "page", mode: "out-in" },
 	},
 	postcss: {
 		plugins: {
