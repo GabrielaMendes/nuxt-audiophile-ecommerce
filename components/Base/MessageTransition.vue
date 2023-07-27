2ba3781d2f4134b8de6653831846d5e1dcc415ba
Gabriela <gdemelo.mendes@gmail.com>
2023-07-27 15:39:13 -0300

    refactor: moved message transition to own component for reusability

---
+++ components/Base/MessageTransition.vue
@@ -0,0 +1,10 @@
+<template>
+	<transition
+		enter-from-class="opacity-0 -translate-y-full"
+		leave-to-class="opacity-0 -translate-y-full"
+		enter-active-class="transition-all duration-300"
+		leave-active-class="transition-all duration-300"
+	>
+		<slot></slot>
+	</transition>
+</template>
