aaeb0defabb5bd07ef90621f764783634b8c5b1b
Gabriela <gdemelo.mendes@gmail.com>
2023-07-27 15:39:39 -0300

    feature: created new icon component

---
+++ components/Icon/Cancel.vue
@@ -0,0 +1,15 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		height="17"
+		viewBox="0 -960 960 960"
+		width="17"
+	>
+		<path 
+      d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"
+      stroke-width="2"
+			fill-rule="evenodd"
+      class="fill-error-red"
+    />
+	</svg>
+</template>
