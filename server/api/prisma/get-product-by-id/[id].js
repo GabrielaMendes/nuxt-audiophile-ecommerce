ac09642bf89e17b45497154b247c45ab2adaca11
Gabriela <gdemelo.mendes@gmail.com>
2023-08-01 17:25:15 -0300

    feature: added error throw in case of product not found

--- server/api/prisma/get-product-by-id/[id].js
+++ server/api/prisma/get-product-by-id/[id].js
@@ -3,9 +3,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-	return await prisma.products.findFirst({
+	const { id } = event.context.params;
+  
+  const product = await prisma.products.findUnique({
 		where: {
-			id: parseInt(event.context.params.id),
+			id: parseInt(id),
 		},
 	});
+
+  if (!product) {
+		throw createError({
+			statusCode: 404,
+			statusMessage: `Product with ID of ${id} does not exist`,
+		});
+	}
+
+  return product
 });
