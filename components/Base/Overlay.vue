0b59d27d59ce3f2d85239452ffad1a248f188b13
Gabriela <gdemelo.mendes@gmail.com>
2023-08-16 19:13:51 -0300

    fix: fixed overlay width

--- components/Base/Overlay.vue
+++ components/Base/Overlay.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-			class="fixed z-[100] left-0 top-0 bg-black/40 w-full h-full text-almost-black"
+			class="fixed z-[100] left-0 top-0 bg-black/40 w-screen h-full text-almost-black"
 	>
     <slot></slot>
   </div>
