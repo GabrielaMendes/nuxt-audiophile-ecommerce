7580052a7d3cb48ca96881dd233a0f18b5be577c
Gabriela <gdemelo.mendes@gmail.com>
2023-08-16 21:01:41 -0300

    fix: fixed section overflow

--- components/BestGear.vue
+++ components/BestGear.vue
@@ -54,7 +54,7 @@ onMounted(() => {
 
 <template>
 	<section
-		class="content-container mb-28 lg:mb-40 flex max-lg:flex-col-reverse items-center"
+		class="content-container mb-28 lg:mb-40 flex max-lg:flex-col-reverse items-center overflow-x-hidden"
 	>
 		<div ref="text" class="flex-1 sm:max-lg:px-16 lg:pr-32 max-lg:text-center">
 			<h2 class="mb-8 max-sm:mt-12 sm:max-lg:mt-16">
